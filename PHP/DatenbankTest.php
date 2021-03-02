162eece5a4531dbf2bfd9748492b2d1d372d537a
Haegger <77164060+Haegger@users.noreply.github.com>
2021-03-02 12:12:02 +0100

    PHP Code

---
+++ PHP/DatenbankTest.php
@@ -0,0 +1,55 @@
+<?php
+$aResult=array("name"=>'',"valid"=>'0');
+if ((@$_REQUEST["RF"]!="")){
+	  $RFID=	trim($_REQUEST["RF"]);
+	  $host_name = 'LOCALHOST';
+	  $database = 'id16262414_hogau';
+	  $user_name = 'Admin1';
+	  $password = 'HogauTestAdmin1!';
+	  
+
+	  $link = new mysqli($host_name, $user_name, $password, $database);
+      
+	  
+	  if ($link->connect_error) {
+		die('<p>Verbindung zum MySQL Server fehlgeschlagen: '. $link->connect_error .'</p>');
+	  } else {
+		
+			$sql = "SELECT `DAYS`, `NAME`,
+			CASE 
+				  WHEN  sysdate() between  `VALIDFROM` and  `VALIDUNTIL` THEN 1
+				  ELSE 0 
+			  END as `intervall` 
+					FROM `RFIDTags` where ID='".$RFID."'" ;
+				//echo $sql;	
+			$result = $link->query($sql);
+
+			if ($result->num_rows > 0) {
+			  while($row = $result->fetch_assoc()) {
+				$valid=$row["intervall"];
+				$weekdays = explode(',', $row["DAYS"]);
+				
+				if (!in_array(date("N"),$weekdays)&& $valid==1){
+					$valid=0;
+				}
+				$aResult=array("name"=>$row["NAME"],"valid"=>$valid);
+				$sql="insert into  `log` (`ID`,`CLIENT`,`ANSWER`, `DATE`)values( '".$RFID."','". @$_SERVER['HTTP_CLIENT_IP']."','".$valid."',sysdate())";
+				//echo $sql;
+				mysqli_query($link, $sql);
+				break;
+			  }
+			} else {
+				$sql="insert into  RFIDTAGS (ID)values( '".$RFID."')";
+				//echo $sql;
+				mysqli_query($link, $sql);
+				$sql="insert into  `log` (`ID`,`CLIENT`,`ANSWER`, `DATE`)values( '".$RFID."','','0',sysdate())";
+			//	echo $sql;
+				mysqli_query($link, $sql);
+				
+			
+			}
+			mysqli_close($link);
+	  }
+}
+echo json_encode($aResult);
+?>
