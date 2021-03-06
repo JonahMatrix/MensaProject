a98a10420bf77a1cc4cc09002954718c546b1f4d
Haegger <77164060+Haegger@users.noreply.github.com>
2021-03-06 15:01:34 +0100

    Log funktioniert zusätzlich

--- PHP/DatenbankTest.php
+++ PHP/DatenbankTest.php
@@ -1,4 +1,7 @@
 <?php
+//print_r($_SERVER);
+//exit;
+
 $aResult=array("name"=>'',"valid"=>'0');
 if ((@$_REQUEST["RF"]!="")){
 	  $RFID =	trim($_REQUEST["RF"]);
@@ -7,14 +10,11 @@ if ((@$_REQUEST["RF"]!="")){
 	  $user_name = 'id16262414_admin1';
 	  $password = 'HogauTestAdmin1!';
 	  
-
 	  $link = new mysqli($host_name, $user_name, $password, $database);
       
-	  
 	  if ($link->connect_error) {
 		die('<p>Verbindung zum MySQL Server fehlgeschlagen: '. $link->connect_error .'</p>');
 	  } else {
-	
 			$sql = "SELECT `DAYS`, `NAME`,`Klasse`,
 			CASE 
 				  WHEN  sysdate() between  `VALIDFROM` and  `VALIDUNTIL` THEN 1
@@ -23,7 +23,6 @@ if ((@$_REQUEST["RF"]!="")){
 					FROM `rfidtags` where ID='".$RFID."'" ;
 				//echo $sql;	
 			$result = $link->query($sql);
-			
 			
 			if ($result->num_rows > 0) {
 			  while($row = $result->fetch_assoc()) {
@@ -34,24 +33,22 @@ if ((@$_REQUEST["RF"]!="")){
 					$valid=0;
 				}
 				$aResult=array("name"=>$row["NAME"],"valid"=>$valid,"klasse"=>$row["Klasse"]);
-				$sql="insert into  `log` (`id`,`CLIENT`,`ANSWER`, `DATE`)values( '".$RFID."','". @$_SERVER['HTTP_CLIENT_IP']."','".$valid."',sysdate())";
+				$sql="insert into  `log` (`id`,`CLIENT`,`ANSWER`, `DATE`)values( '".$RFID."','".@$_SERVER['REMOTE_ADDR']."','".$valid."',sysdate())";
 				//echo $sql;
 				mysqli_query($link, $sql);
 				break;
 			 }
 			} else {
-				$sql="insert into  RFIDTAGS (ID)values( '".$RFID."')";
+				$sql="insert into  rfidtags (ID)values( '".$RFID."')";
 				//echo $sql;
 				mysqli_query($link, $sql);
-				$sql="insert into  `log` (`id`,`CLIENT`,`ANSWER`, `DATE`)values( '".$RFID."','','0',sysdate())";
-			//	echo $sql;
+				$sql="insert into  `log` (`id`,`CLIENT`,`ANSWER`, `DATE`)values( '".$RFID."','".@$_SERVER['REMOTE_ADDR']."','0',sysdate())";
+		  	    //echo $sql;
 				mysqli_query($link, $sql);
-				
-			
+	
 			}
-		 
 			mysqli_close($link);
-	  }
-}
+	    }
+    }
 echo json_encode($aResult);
 ?>
