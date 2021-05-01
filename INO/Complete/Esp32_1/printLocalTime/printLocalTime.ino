bdf3e881c2ef850c3a5d3ce4fe0712f05e0928aa
JonahMatrix <46624493+JonahMatrix@users.noreply.github.com>
2021-05-01 14:49:20 +0200

    deleted univoled files

--- INO/Complete/Esp32_1/printLocalTime/printLocalTime.ino
+++
@@ -1,9 +0,0 @@
-void printLocalTime()
-{
-  struct tm timeinfo;
-  if(!getLocalTime(&timeinfo)){
-    Serial.println("Failed to obtain time");
-    return;
-  }
-  Serial.println(&timeinfo, "%A, %B %d %Y %H:%M:%S");
-}
